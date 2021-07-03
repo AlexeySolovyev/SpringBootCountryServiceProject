92f495c657a2d76567e1966b4c7181e2f1538a25
Alexey Solovyev <solovevalex@gmail.com>
2021-07-03 19:26:41 -0400

    Project structure has been created

--- src/main/java/com/restservices/springbootcountryserviceproject/SpringBootCountryServiceProjectApplication.java
+++ src/main/java/com/restservices/springbootcountryserviceproject/SpringBootCountryServiceProjectApplication.java
@@ -8,6 +8,6 @@ public class SpringBootCountryServiceProjectApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootCountryServiceProjectApplication.class, args);
-    }
 
+    }
 }
